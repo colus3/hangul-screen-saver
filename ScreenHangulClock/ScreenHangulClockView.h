9fa592d983d72da00d4ea7ef4344e334a293c4ba
visu4l <kchclub3@seworks.co>
2015-09-11 16:55:41 +0900

    Initial Commit

---
+++ ScreenHangulClock/ScreenHangulClockView.h
@@ -0,0 +1,13 @@
+//
+//  ScreenHangulClockView.h
+//  ScreenHangulClock
+//
+//  Created by visu4l on 2015. 9. 11..
+//  Copyright (c) 2015년 visu4l. All rights reserved.
+//
+
+#import <ScreenSaver/ScreenSaver.h>
+
+@interface ScreenHangulClockView : ScreenSaverView
+
+@end
